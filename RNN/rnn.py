83a86a114ec48cb4843bf010607dcb4387bdc613
kjo26619 <32588798+kjo26619@users.noreply.github.com>
2020-10-22 17:30:48 +0900

    Update rnn.py

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 def lstm(input_feature=1000, output_feature = 46):
     model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(input_feature, 120),
-        tf.keras.layers.LSTM(120),
+        tf.keras.layers.Embedding(input_feature, 80),
+        tf.keras.layers.LSTM(80),
         tf.keras.layers.Dense(output_feature, activation='sigmoid')
     ])
 
